523b473c04421e951c8e54fe127e57649e509a79
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-07-19 08:47:37 +0200

    Add primary services

---
+++ edge-service/src/main/java/com/diabeaten/edgeservice/EdgeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.diabeaten.edgeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdgeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdgeServiceApplication.class, args);
+	}
+
+}
