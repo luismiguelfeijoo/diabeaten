c118b1f08c48935bf12206f604f5f0e999edf58a
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-07-22 13:28:20 +0200

    Update bolus calc

--- edge-service/src/main/java/com/diabeaten/edgeservice/util/IOBCalculator.java
+++ edge-service/src/main/java/com/diabeaten/edgeservice/util/IOBCalculator.java
@@ -16,24 +16,22 @@ public class IOBCalculator {
         System.out.println("end: " +end);
         long minsAgo = DateOperations.minutesBetween(prevBolusDate, currentBolusDate);
         System.out.println(minsAgo);
-        //BigDecimal tau = (peak.multiply(BigDecimal.ONE.subtract(peak.divide(end, 9, RoundingMode.HALF_DOWN)).divide(BigDecimal.ONE.subtract((BigDecimal.valueOf(2).multiply(peak).divide(end,2, RoundingMode.HALF_EVEN))), 2, RoundingMode.HALF_EVEN))); // time constant of exponential decay
         BigDecimal tau = peak.multiply((BigDecimal.ONE.subtract(peak.divide(end, 5, RoundingMode.HALF_EVEN))).divide(BigDecimal.ONE.subtract(BigDecimal.valueOf(2).multiply(peak.divide(end, 5, RoundingMode.HALF_EVEN))), 5, RoundingMode.HALF_EVEN));
         System.out.println("tau: " +tau);
         BigDecimal a = new BigDecimal("2").multiply(tau).divide(end, 2, RoundingMode.HALF_EVEN); // rise time factor
         System.out.println("a: " +a);
         BigDecimal S = BigDecimal.ONE.divide(BigDecimal.ONE.subtract(a).add((BigDecimal.ONE.add(a)).multiply( BigDecimal.valueOf(Math.exp( (end.negate().divide(tau, 5, RoundingMode.HALF_EVEN)).doubleValue())))), 5, RoundingMode.HALF_EVEN);
-        //BigDecimal S = BigDecimal.ONE.divide(BigDecimal.ONE.subtract(a).add(BigDecimal.ONE.add(a).multiply(BigDecimal.valueOf(Math.exp(end.negate().divide(tau, 5 , RoundingMode.HALF_EVEN).doubleValue())))),5, RoundingMode.HALF_EVEN);
         System.out.println("s: " +S);
         //BigDecimal result = insulinUsed.multiply(BigDecimal.ONE.subtract(S.multiply(BigDecimal.ONE.subtract(a)).multiply((BigDecimal.valueOf(minsAgo).pow(2)).divide(tau.multiply(end).multiply(BigDecimal.ONE.subtract(a)), 2, RoundingMode.HALF_EVEN).subtract(BigDecimal.valueOf(minsAgo).divide(tau, 2, RoundingMode.HALF_EVEN).subtract(BigDecimal.ONE))).multiply(BigDecimal.valueOf(Math.exp(BigDecimal.valueOf(minsAgo).negate().divide(tau, 2, RoundingMode.HALF_EVEN).doubleValue())).add(BigDecimal.ONE))));
         System.out.println("insulin: " +insulinUsed);
         BigDecimal iob = BigDecimal.ONE.subtract(S.multiply(BigDecimal.ONE.subtract(a)).multiply(  (( BigDecimal.valueOf(Math.pow(minsAgo, 2)).divide(tau.multiply(end).multiply(BigDecimal.ONE.subtract(a)), 5, RoundingMode.HALF_EVEN).subtract(BigDecimal.valueOf(minsAgo).divide(tau, 5, RoundingMode.HALF_EVEN)).subtract(BigDecimal.ONE)   ).multiply(  BigDecimal.valueOf(Math.exp(BigDecimal.valueOf(minsAgo).negate().divide(tau, 5, RoundingMode.HALF_EVEN).doubleValue())    ))).add(BigDecimal.ONE) ));
-        // S.multiply(BigDecimal.ONE.subtract(a)).multiply( (BigDecimal.valueOf(minsAgo).divide(tau.multiply(end).multiply(BigDecimal.ONE.subtract(a)), 5, RoundingMode.HALF_EVEN).subtract(BigDecimal.valueOf(minsAgo).divide(tau, 5, RoundingMode.HALF_EVEN)).subtract(BigDecimal.ONE)).multiply(BigDecimal.valueOf(Math.exp(BigDecimal.valueOf(minsAgo).negate().divide(tau, 5, RoundingMode.HALF_EVEN).doubleValue()))  ).add(BigDecimal.ONE) ).negate().add(BigDecimal.ONE)
         System.out.println("iob: " +iob);
         BigDecimal result = insulinUsed.multiply(iob);
         System.out.println("result: " +result);
         return result;
-
+        //BigDecimal S = BigDecimal.ONE.divide(BigDecimal.ONE.subtract(a).add(BigDecimal.ONE.add(a).multiply(BigDecimal.valueOf(Math.exp(end.negate().divide(tau, 5 , RoundingMode.HALF_EVEN).doubleValue())))),5, RoundingMode.HALF_EVEN);
         //BigDecimal result = insulinUsed.multiply(BigDecimal.ONE.subtract(S.multiply(BigDecimal.ONE.subtract(a)).multiply((BigDecimal.valueOf(minsAgo).pow(2)).divide(tau.multiply(end).multiply(BigDecimal.ONE.subtract(a)), 2, RoundingMode.HALF_EVEN).subtract(BigDecimal.valueOf(minsAgo).divide(tau, 2, RoundingMode.HALF_EVEN).subtract(BigDecimal.ONE))).multiply(BigDecimal.valueOf(Math.exp(BigDecimal.valueOf(minsAgo).negate().divide(tau, 2, RoundingMode.HALF_EVEN).doubleValue())).add(BigDecimal.ONE))));
-
+        // S.multiply(BigDecimal.ONE.subtract(a)).multiply( (BigDecimal.valueOf(minsAgo).divide(tau.multiply(end).multiply(BigDecimal.ONE.subtract(a)), 5, RoundingMode.HALF_EVEN).subtract(BigDecimal.valueOf(minsAgo).divide(tau, 5, RoundingMode.HALF_EVEN)).subtract(BigDecimal.ONE)).multiply(BigDecimal.valueOf(Math.exp(BigDecimal.valueOf(minsAgo).negate().divide(tau, 5, RoundingMode.HALF_EVEN).doubleValue()))  ).add(BigDecimal.ONE) ).negate().add(BigDecimal.ONE)
+        //BigDecimal tau = (peak.multiply(BigDecimal.ONE.subtract(peak.divide(end, 9, RoundingMode.HALF_DOWN)).divide(BigDecimal.ONE.subtract((BigDecimal.valueOf(2).multiply(peak).divide(end,2, RoundingMode.HALF_EVEN))), 2, RoundingMode.HALF_EVEN))); // time constant of exponential decay
     }
 }
