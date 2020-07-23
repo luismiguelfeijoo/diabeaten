e74903f9f6b7c1f4cd9f332de7a9f489522da575
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-07-23 14:30:27 +0200

    Add manual glucose and bolus forms

--- diabeaten-ui/src/app/pages/registry/registry.component.ts
+++ diabeaten-ui/src/app/pages/registry/registry.component.ts
@@ -6,6 +6,14 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Glucose } from 'src/app/_models/glocuse';
 import { environment } from 'src/environments/environment';
 import { Bolus } from 'src/app/_models/bolus';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {
+  FormControl,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
+import { createOfflineCompileUrlResolver } from '@angular/compiler';
 
 @Component({
   selector: 'app-registry',
@@ -15,10 +23,31 @@ import { Bolus } from 'src/app/_models/bolus';
 export class RegistryComponent implements OnInit {
   glucoseList: Glucose[];
   bolusList: Bolus[];
+  modalRefence;
+
+  addGlucoseForm: FormGroup = new FormGroup({
+    glucose: new FormControl(),
+  });
+
+  addBolusForm: FormGroup = new FormGroup({
+    glucose: new FormControl(),
+    chBolus: new FormControl(),
+    correctionBolus: new FormControl(),
+  });
+
+  open(content) {
+    this.modalRefence = this.modalService.open(content, {
+      ariaLabelledBy: 'modal-basic-title',
+    });
+    this.modalRefence.result.then((result) => {
+      console.log(`Closed with: ${result}`);
+    });
+  }
 
   constructor(
+    private modalService: NgbModal,
     private toastr: ToastrService,
-    private router: Router,
+    private formBuilder: FormBuilder,
     private authenticationService: AuthenticationService,
     private http: HttpClient
   ) {}
@@ -29,6 +58,7 @@ export class RegistryComponent implements OnInit {
       Authorization: `Basic ${this.authenticationService.userValue.authdata}`,
     }),
   };
+
   ngOnInit(): void {
     this.http
       .get<Glucose[]>(
@@ -58,6 +88,82 @@ export class RegistryComponent implements OnInit {
           this.bolusList = data.sort(
             (a, b) => new Date(a.date).getTime() + new Date(b.date).getTime()
           );
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+
+    this.addGlucoseForm = this.formBuilder.group({
+      glucose: ['', [Validators.required, Validators.min(0)]],
+    });
+
+    this.addBolusForm = this.formBuilder.group({
+      glucose: ['', [Validators.required, Validators.min(0)]],
+      chBolus: ['', [Validators.required, Validators.min(0)]],
+      correctionBolus: ['', Validators.required],
+    });
+  }
+
+  addGlucose(): void {
+    if (this.addGlucoseForm.invalid) {
+      return;
+    }
+    const newGlucose: Glucose = {
+      glucose: this.addGlucoseForm.controls.glucose.value,
+      date: new Date(),
+    };
+    this.http
+      .post<Glucose>(
+        `${environment.apiUrl}/patients/${this.authenticationService.userValue.id}/glucose`,
+        newGlucose,
+        this.httpOptions
+      )
+      .subscribe(
+        (data) => {
+          this.glucoseList.push(data);
+          this.glucoseList.sort(
+            (a, b) => new Date(a.date).getTime() + new Date(b.date).getTime()
+          );
+          this.modalRefence.close();
+          this.addGlucoseForm = this.formBuilder.group({
+            glucose: ['', [Validators.required, Validators.min(0)]],
+          });
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  addBolus(): void {
+    if (this.addBolusForm.invalid) {
+      return;
+    }
+    const newBolus: Bolus = {
+      glucose: this.addBolusForm.controls.glucose.value,
+      chBolus: this.addBolusForm.controls.chBolus.value,
+      correctionBolus: this.addBolusForm.controls.correctionBolus.value,
+      date: new Date(),
+    };
+    this.http
+      .post<Bolus>(
+        `${environment.apiUrl}/patients/${this.authenticationService.userValue.id}/bolus`,
+        newBolus,
+        this.httpOptions
+      )
+      .subscribe(
+        (data) => {
+          this.bolusList.push(data);
+          this.bolusList.sort(
+            (a, b) => new Date(a.date).getTime() + new Date(b.date).getTime()
+          );
+          this.modalRefence.close();
+          this.addBolusForm = this.formBuilder.group({
+            glucose: ['', [Validators.required, Validators.min(0)]],
+            chBolus: ['', [Validators.required, Validators.min(0)]],
+            correctionBolus: ['', Validators.required],
+          });
         },
         (error) => {
           console.log(error);
