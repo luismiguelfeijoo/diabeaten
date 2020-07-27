4896b2dc1ef05efeeda3fd8ad94568893a94b443
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-07-27 10:14:39 +0200

    Update timezone

--- glucose-bolus-service/src/main/java/com/diabeaten/glucosebolusservice/GlucoseBolusServiceApplication.java
+++ glucose-bolus-service/src/main/java/com/diabeaten/glucosebolusservice/GlucoseBolusServiceApplication.java
@@ -4,12 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 public class GlucoseBolusServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GlucoseBolusServiceApplication.class, args);
+	}
+
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 }
